d7ebd9a981b88d3dae58784809bbb0919fe241f9
Justin Italiano <jitalian@seas.upenn.edu>
2023-10-06 17:30:24 -0400

    Custom cursor, projects page, contact page.

---
+++ cursor.js
@@ -0,0 +1,7 @@
+const cursor = document.querySelector('.cursor');
+cursor.style.display = 'none'
+document.addEventListener('mousemove', (e) => {
+    cursor.style.display = 'block';
+    cursor.style.left = e.clientX + 'px';
+    cursor.style.top = e.clientY + 'px';
+})
