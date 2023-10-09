09e711b4026242120506a0255e2d5094eae6cd28
Justin Italiano <jitalian@seas.upenn.edu>
2023-10-09 18:10:00 -0400

    Cursor debug, contact form thanks page

--- cursor.js
+++ cursor.js
@@ -1,7 +1,12 @@
 const cursor = document.querySelector('.cursor');
 cursor.style.display = 'none'
-document.addEventListener('mousemove', (e) => {
-    cursor.style.display = 'block';
-    cursor.style.left = e.clientX + 'px';
-    cursor.style.top = e.clientY + 'px';
-})
+
+if ('ontouchstart' in window) {
+    document.querySelector('.cursor').style.display = 'none'
+} else {
+    document.addEventListener('mousemove', (e) => {
+        cursor.style.display = 'block';
+        cursor.style.left = e.clientX + 'px';
+        cursor.style.top = e.clientY + 'px';
+    })
+}
